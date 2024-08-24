b8208744b959fd394a04e63ce11c0b2f202c4578
Kyrushiqi <Karinalam39@gmail.com>
2024-08-24 18:25:56 -0400

    Initial commit

---
+++ src/main/java/net/kyrushiqi/tutorialmod/TutorialMod.java
@@ -0,0 +1,20 @@
+package net.kyrushiqi.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod"; // unique identifier for your mod, uppercase or spaces not allowed
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
